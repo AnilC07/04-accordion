ae5c9951b440863a66ea7387e1669f037da3c0c8
Anil CELIK <96571618+AnilTheKillerCoder@users.noreply.github.com>
2023-01-31 22:38:27 +0100

    start and finish accordion

--- src/Question.js
+++ src/Question.js
@@ -1,7 +1,22 @@
-import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+
+const Question = ({title, info}) => {
+  const [show, setShow] = useState(false);
+
+
+  return (
+    <div className="question">
+      <header>
+        <h4>{title}</h4>
+        <div className="btn" onClick={()=>setShow(!show)}>
+          {!show ? <AiOutlinePlus />:<AiOutlineMinus />}
+        </div>
+      </header>
+      {show && <p>{info}</p>}
+      
+    </div>
+  );
 };
 
 export default Question;
